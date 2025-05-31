8dac6044e494193f3ed5277ccb03f22d7810422a
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-31 14:09:53 +0900

    【add】Components

--- my_server.py
+++ my_server.py
@@ -1,11 +1,38 @@
 import asyncio
 from fastmcp import FastMCP
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP(
+    # 名前を付けることでクライアント側やログからサーバーを特定するのに役立つ
+    name="HelpfulAssistant",
+    # 引数instructionsではサーバーとのやり取り方法についての指示を指定出来る
+    instructions="""
+        This server provides data analysis tools.
+        Call get_average() to analyze numerical data.
+        """
+)
 
 @mcp.tool()
 def greet(name: str) -> str:
     return f"Hello, {name}!"
+
+
+# [Components]
+
+# Resource Templates
+@mcp.prompt()
+def analyze_data(data_points: list[float]) -> str:
+    """Creates a prompt asking for analysis of numerical data."""
+    formatted_data = ", ".join(str(point) for point in data_points)
+    return f"Please analyze these data points: {formatted_data}"
+
+# Prompts
+""""LLMに渡したいデータをクライアントから引数で受け取る"""
+@mcp.prompt()
+def analyze_data(data_points: list[float]) -> str:
+    """Creates a prompt asking for analysis of numerical data."""
+    formatted_data = ", ".join(str(point) for point in data_points)
+    return f"Please analyze these data points: {formatted_data}"
+
 
 if __name__ == "__main__":
     mcp.run()
